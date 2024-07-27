563ee435cdaf399c4a12a3087c61a6c7785c2ced
mountainricardo <mountainricardo@gmail.com>
2024-07-27 13:28:45 +0200

    Copy of GitLab project
    GitLab project (https://gitlab.com/texteditor/web-application) was aasigned to me as maintainer in 2020, so this copy to keep the frontend code I created there from the scratch.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/html-self-closing': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
